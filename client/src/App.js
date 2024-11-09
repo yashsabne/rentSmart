92c7f6e0ea3a3b9c6e714b447d012ca4af930cd4
Yash Sabne <yashsabne39@gmail.com>
2024-11-09 15:53:51 +0530

    added the vercel analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
@@ -13,12 +14,14 @@ import Dashboard from "./pages/Dashboard";
 import PropertyBuyPage from "./pages/PropertyBuyPage";
 import ContactPropertyOwner from "./pages/ContactPropertyOwner";
 import ChatPage from "./pages/Chat";
-import BeaPremium from "./pages/BeAPremium"
+import BeaPremium from "./pages/BeAPremium";
 import PaymentDetails from "./pages/PaymentDetails";
 import HelpAboutPage from "./pages/Help";
 import ForgotPassword from "./pages/ForgetPage";
 import ResetPassword from "./pages/ResetPage";
 import NotFound from "./pages/Notfound"; 
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
     <div>
@@ -32,21 +35,20 @@ function App() {
           <Route path="/properties/category/:category" element={<CategoryPage />} />
           <Route path="/properties/search/:search" element={<SearchPage />} />
           <Route path="/:userId/properties" element={<PropertyList />} /> 
-          <Route path="/successfully-listed-property" element={<SuccessPage/>} />
+          <Route path="/successfully-listed-property" element={<SuccessPage />} />
           <Route path="/dashboard" element={<Dashboard />} /> 
-          <Route path="/property-search" element={<PropertyBuyPage/>}/>
-          <Route path='/users/contact-property-owner' element={<ContactPropertyOwner/>} />
-          {/* <Route path="/chat-with-owner" element={<ChatPage/>} /> */}
-          <Route path="/users/discover-premium-version" element={<BeaPremium/>} />
-          <Route path="/help" element={<HelpAboutPage/>} />
-          <Route path="/payment/details" element={<PaymentDetails/>} />
-          <Route path="/forgot-password" element={<ForgotPassword/> } />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="*" element={<NotFound />} />
-
+          <Route path="/property-search" element={<PropertyBuyPage />} />
+          <Route path="/users/contact-property-owner" element={<ContactPropertyOwner />} />
+          {/* <Route path="/chat-with-owner" element={<ChatPage />} /> */}
+          <Route path="/users/discover-premium-version" element={<BeaPremium />} />
+          <Route path="/help" element={<HelpAboutPage />} />
+          <Route path="/payment/details" element={<PaymentDetails />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Analytics /> {/* Add the Analytics component here */}
       </BrowserRouter>
- 
     </div>
   );
 }
