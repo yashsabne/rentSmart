a98d65b0f7f3ba8687a8ecbe8e271d63ed91183c
Yash Sabne <138506637+yashsabne@users.noreply.github.com>
2024-11-26 17:19:48 +0530

    Update Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
   useEffect(() => { 
     const timer = setTimeout(() => {
       setShowButton(false); 
-    }, 2000);
+    }, 4000);
  
     return () => clearTimeout(timer);
   }, []);
@@ -18,7 +18,7 @@ const Loader = () => {
         <div className="cube"></div>
 
         {showButton && 
-        <div className="loading-message">loading..</div>
+        <div className="loading-message">loading, you may need to wait because of free M0 database..</div>
         }
 
       </div>
